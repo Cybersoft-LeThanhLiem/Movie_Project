8bcd39ba49d0c7e85fdd4ddeffc98e1ef14612ac
Liem <leliem28@gmail.com>
2021-08-16 19:41:11 +0700

    Clear old imports

--- src/App.js
+++ src/App.js
@@ -1,23 +1,4 @@
 import './App.css';
-import BaiTapLayoutTailwind from './TailWindComponent/BaiTapLayoutTailwind';
-import BorderRadiusDemo from './TailWindComponent/BorderRadiusDemo';
-import CustomCSS from './TailWindComponent/CustomCSS';
-import DemoGrid from './TailWindComponent/DemoGrid';
-import FlexDemo from './TailWindComponent/FlexDemo';
-import ChunkLodash from './Lodash/ChunkLodash';
-import CompareObjectArray from './Lodash/CompareObjectArray';
-import FillLodash from './Lodash/FillLodash';
-import FlattenLodash from './Lodash/FlattenLodash';
-import IncludeLodash from './Lodash/IncludeLodash';
-import JoinDemo from './Lodash/JoinDemo';
-import LastFirstLodash from './Lodash/LastFirstLodash';
-import SortLodash from './Lodash/SortLodash';
-import UniqLodash from './Lodash/UniqLodash';
-import PaddingMarginDemo from './TailWindComponent/PaddingMarginDemo';
-import ResponsiveDemo from './TailWindComponent/ResponsiveDemo';
-import TextBackgroundDemo from './TailWindComponent/TextBackgroundDemo';
-import WidthHeightDemo from './TailWindComponent/WidthHeightDemo';
-import BaiTapTongHop from './BaiTapTongHop/BaiTapTongHop';
 
 function App() {
   return (
